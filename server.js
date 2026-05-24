da182363789c040bb5d0600c5157d989599e8816
anurajrijal <anurajrijal@gmail.com>
2026-05-24 17:43:18 +0545

    implement basic HTTP server with file serving capabilities

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
-const http = require('http')
-const fs= require(fs)
-const path = 
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : "req.url",
+  );
+
+  const extName = path.extname(filePath).toLowerCase();
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath,(err, content)=>{
+    if(err){
+        if(err.code === "ENOENT"){
+            res.writeHead(404,{"content-type":"text/html"})
+            res.end("404: File NOt Found")
+        }
+    }else{
+        res.writeHead(200, {"content-type":contentType})
+        res.end(content, 'utf-8')
+    }
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
